e288371fa92c1f08725033354851f16984f5cf32
ElQwaqu <cobbinaheldridge@gmail.com>
2025-05-25 02:37:10 +0000

    Updated Multi C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,8 @@
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x) || x <= 0) {
+
+    if (x <= 0) return;
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < x; i++) {
